59eff9371793d054ec52b0a2dccf56b8081e6f6b
lalinthorn <lalinpye@gmail.com>
2022-08-03 13:50:54 +0700

    1

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -2,50 +2,59 @@ import java.util.*;
 
 public class NeuralNetwork {
     ArrayList<Double[]> Train_dataset = new ArrayList<>();
-    ArrayList<Double> Train_desired = new ArrayList<>();
-    int Neural[] = {8, 4, 1};
-    Double Node[][] = new Double[Neural.length][]; //[layer][node]
-    Double Local_gradient_node[][] = new Double[Neural.length][];
-    Double AVG_error;
-    Double Min_error;
-    int Max_Epoch;
-    Double biases;
-    Matrix[] Layerweight;
-    Matrix[] Weightchange;
+    ArrayList<Double[]> Train_desired = new ArrayList<>();
+    int Layer[] = {8, 4, 1};
+    Double Desired[] = new Double[Layer[Layer.length-1]];
+    Double Node[][] = new Double[Layer.length][]; //[layer][node]
+    Double LocalGradient[][] = new Double[Layer.length][];
+    Double Error[] = new Double[Layer[Layer.length-1]];
+    Double AVGError = 1000.0;
+    Double MinError = 0.00001;
+    Double MaxEpoch = 1000.0;
+    Double Biases = 1.0;
+    Double n =0.001;
+    Matrix[] LayerWeight = new Matrix[Layer.length - 1];
+    Matrix[] WeightChange = new Matrix[Layer.length - 1];
 
 
     public NeuralNetwork(ArrayList data, ArrayList desired) {
         Train_dataset = data;
         Train_desired = desired;
 
-        for (int i = 0; i < Neural.length; i++) {
-            Node[i] = new Double[Neural[i]];
-            Local_gradient_node[i] = new Double[Neural[i]];
+         //node
+        for (int i = 0; i < Layer.length; i++) { //0 1 2
+            Node[i] = new Double[Layer[i]];
+            LocalGradient[i] = new Double[Layer[i]];
         }
 
-        Layerweight = new Matrix[Neural.length - 1];
-        Weightchange = new Matrix[Neural.length - 1];
-        for (int layer = 0; layer < Layerweight.length; layer++) {
-            Layerweight[layer] = new Matrix(Neural[layer + 1], Neural[layer], true);
-            Weightchange[layer] = new Matrix(Neural[layer + 1], Neural[layer], false);
+        //weight
+        for (int layer = 0; layer < LayerWeight.length; layer++) { // 0 1
+            LayerWeight[layer] = new Matrix(Layer[layer + 1], Layer[layer], true);
+            WeightChange[layer] = new Matrix(Layer[layer + 1], Layer[layer], false);
         }
 
     }
 
     public void train() {
         int N = 0;
-        while (N < Max_Epoch && AVG_error > Min_error) {
+        while (N < MaxEpoch && AVGError > MinError) {
             for (int loop = 0; loop < Train_dataset.size(); loop++) {
                 int Ran = (int) (Math.random() * Train_dataset.size());
 
-                for (int i = 0; i < Neural[0]; i++) {
+                for(int i = 0; i< Layer[Layer.length-1]; i++) {
+                    Desired[i] = Train_desired.get(Ran)[i];
+                }
+
+                for (int i = 0; i < Layer[0]; i++) {
                     Node[0][i] = Train_dataset.get(Ran)[i];
                 }
 
                 forward_pass();
                 find_error();
                 backward_pass();
+                changeweight();
 
+                System.out.println(Desired[0]*800 + "  " + activation_fn(Node[2][0])*800);
             }
 
             N++;
@@ -54,41 +63,77 @@ public class NeuralNetwork {
     }
 
     public void forward_pass() {
-        for (int layer = 0; layer < Neural.length - 1; layer++) {
-
-            if (Layerweight[layer].cols != Node[layer].length) {
-                System.out.println("invalid");
-                return;
+        for(int l = 0; l < Layer.length-1; l++){  //layer 0 1
+            for(int n = 0; n < Node[l+1].length; n++){ //next node 1 2
+                double sum = 0.0;
+                for(int w = 0; w < Node[l].length; w++){ //node
+                    sum += (LayerWeight[l].data[n][w]*activation_fn(Node[l][w]));
+                }
+                Node[l+1][n] = sum+Biases;
             }
+        }
+    }
 
-            Double sum_input;
-            Double[] sum_inputnode = new Double[Neural[layer + 1]];
+    public void find_error() {
+        for(int i=0; i < Layer[Layer.length-1]; i++){
+             Error[i] = Desired[i] - Node[Layer.length-1][i];
+             LocalGradient[Layer.length-1][i] = Error[i];
+        }
+    }
 
-
-            for (int j = 0; j < Neural[layer + 1]; j++) {
-                double sum = 0;
-
-                for (int k = 0; k < Node[layer].length; k++) sum += Layerweight[layer].data[j][k]*activation_fn(Node[layer][k]);
-
-                sum_input = sum + biases;
-                sum_inputnode[j] = sum_input;
+    public void backward_pass() {
+        //output layer
+        int hidden = Layer.length - 2;
+        int out = Layer.length - 1;
+        for(int o = 0; o <Layer[out]; o++) { // loop node of output layer
+            for (int h = 0; h < Layer[hidden]; h++) { // loop node of last hidden layer
+                double wc = n * Error[o] * activation_fn(Node[hidden][h]);
+                WeightChange[hidden].set(o, h, wc);
             }
+        }
 
-            Node[layer + 1] = sum_inputnode;
+        //hidden layer
+        for(int i = hidden; i>0; i--){ // loop hidden layer
+            for (int h = 0; h <Layer[i]; h++){ //  loop node in each hidden
+                double sum =0.0;
+                for(int n = 0; n<Layer[i+1]; n++){ // loop next layer
+                    sum += LayerWeight[i].data[n][h]*LocalGradient[i+1][n];
+                }
+                LocalGradient[i][h] = activation_fn_diff(Node[i][h])*sum;
+            }
+        }
+
+        //inputt layer
+        for(int i = 0; i<Layer.length-2; i++){ // loop layer from input
+            for(int j = 0; j <Layer[i]; j++){ // loop node of each layer
+                for(int h = 0; h<Layer[i+1]; h++){ // loop node of next layer
+                    double wc = n * LocalGradient[i+1][h] * activation_fn(Node[i][j]);
+                    WeightChange[i].set(h, j,wc);
+                }
+            }
         }
 
     }
 
-    public void find_error() {
-
-    }
-
-    public void backward_pass() {
-
+    public void changeweight() {
+        for(int l = 0; l < LayerWeight.length; l++) { // loop layer
+            for(int i = 0; i <Layer[l]; i++){ // loop node
+                for(int j = 0; j<Layer[l+1]; j++){ // loop next node
+                    double wc = LayerWeight[l].data[j][i] + WeightChange[l].data[j][i];
+                    LayerWeight[l].set(j,i,wc);
+                }
+            }
+        }
     }
 
     public Double activation_fn(Double v){
 
-        return 0.0;
+        return Math.max(0.01,v);
     }
+
+    public Double activation_fn_diff(Double v){
+        if (v<=0) return 0.01;
+        else return 1.0;
+    }
+
 }
--- src/mainproject.java
+++ src/mainproject.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class mainproject {
     public static void main(String[] args) {
 
-        ArrayList<Double[]> Train_dataset = new ArrayList<Double[]>();
-        ArrayList<Double> Train_desired = new ArrayList<Double>();
-        ArrayList<Double[]> Test_dataset = new ArrayList<Double[]>();
-        ArrayList<Double> Test_desired = new ArrayList<Double>();
+        ArrayList<Double[]> Train_dataset = new ArrayList<>();
+        ArrayList<Double[]> Train_desired = new ArrayList<>();
+        ArrayList<Double[]> Test_dataset = new ArrayList<>();
+        ArrayList<Double[]> Test_desired = new ArrayList<>();
 
         //read file
         try {
@@ -21,28 +21,31 @@ public class mainproject {
 
                 //devide train and test
                 if(line %10 == 0) {
+                    Double[] arraydata = new Double[8];
+                    Double[] arraydesired = new Double[1];
                     for (String a : dataline) {
-
-                        Double[] arraydata = new Double[8];
                         if (i == 8){
-                            Test_desired.add(Double.parseDouble(a));
+                            arraydesired[0] = Double.parseDouble(a)/500;
+                            Test_desired.add(arraydesired);
                             Test_dataset.add(arraydata);
                         }
-                        else arraydata[i] = Double.parseDouble(a) ;
+                        else arraydata[i] = Double.parseDouble(a)/500;
 
                         i++;
                     }
-                }else for (String a : dataline) {
-
+                }else{
                     Double[] arraydata = new Double[8];
+                    Double[] arraydesired = new Double[1];
+                    for (String a : dataline) {
+                        if(i==8){
+                            arraydesired[0] = Double.parseDouble(a)/800;
+                            Train_desired.add(arraydesired);
+                            Train_dataset.add(arraydata);
+                        }
+                        else arraydata[i] = Double.parseDouble(a)/800 ;
 
-                    if(i==8){
-                        Train_desired.add(Double.parseDouble(a));
-                        Train_dataset.add(arraydata);
+                        i++;
                     }
-                    else arraydata[i] = Double.parseDouble(a) ;
-
-                    i++;
                 }
 
                 line++;
@@ -51,6 +54,7 @@ public class mainproject {
             myReader.close();
 
             NeuralNetwork nn = new NeuralNetwork(Train_dataset,Train_desired);
+            nn.train();
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
